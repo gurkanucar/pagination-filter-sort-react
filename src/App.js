30dfa6a0089ae8c2e5e159920ebbb236f88b31ee
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-23 13:53:32 +0300

    App wrapped with router and queryClient

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { Routes, Route } from "react-router-dom";
 import SearchPage from "./SearchPage";
 import SearchPage2 from "./SearchPage2";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      cacheTime: Infinity,
-    },
-  },
-});
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/search2" element={<SearchPage2 />} />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <>
+      <Routes>
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/search2" element={<SearchPage2 />} />
+      </Routes>
+    </>
   );
 }
 
